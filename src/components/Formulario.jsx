a7495a927f061900546d70fdc0510fc61193a9e0
Gian Marco Pablo Totocayo <gianmarcopablo588@gmail.com>
2023-06-04 00:38:50 -0500

    second  commit

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,4 +1,7 @@
+import { useEffect,useState } from "react"
 import styled from "@emotion/styled"
+import useSelectModenas from "../hooks/useSelectModenas"
+import { monedas } from "../data/monedas"
 
 const InputSubmit = styled.input `
     background-color: #9497ff;
@@ -11,6 +14,7 @@ const InputSubmit = styled.input `
     font-size: 20px;
     border-radius: 5px;
     transition: background-color .3s ease;
+    margin-top: 30px;
     &:hover {
         background-color: #7a7dfe;
         cursor: pointer;
@@ -18,8 +22,34 @@ const InputSubmit = styled.input `
 `
 
 const Formulario = () => {
+
+  const [criptos,setCriptos] = useState([])
+
+  const [moneda,SelectMonedas] = useSelectModenas("Elige tu moneda",monedas)
+  const [criptomoneda,SelectCriptomoneda] = useSelectModenas("Elige tu Criptomoneda",criptos)
+
+  useEffect(() => {
+    const consultarApi = async () =>{
+      const url = "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD"
+      const respuesta = await fetch(url)
+      const resultado = await respuesta.json()
+      const arrayCriptos = resultado.Data.map( cripto=>{
+        const objeto = {
+          id: cripto.CoinInfo.Name,
+          nombre: cripto.CoinInfo.FullName
+        }
+        return objeto
+      })
+
+      setCriptos(arrayCriptos)
+    }
+    consultarApi()
+  }, [])
+  
   return (
     <form>
+        <SelectMonedas/>
+        <SelectCriptomoneda/>
         <InputSubmit 
             type="submit"
             value="Cotizar"
